16d04f1b6a448f3561888d2dc6bcacc84df2f84a
HanJaeYun <danbidad@gmail.com>
2021-09-25 14:45:36 +0900

    최초 기동시 에러 수정 mongodb 접속을 먼저 시도함

--- website/server/server.js
+++ website/server/server.js
@@ -7,11 +7,14 @@ import logger from './libs/logger';
 // Must come before attach middlewares so Mongoose validations can use translations
 import './libs/i18n';
 
-import attachMiddlewares from './middlewares/index';
-
 // Load config files
 import './libs/setupMongoose';
+
+// jyhan 매우 중요한 수정임 도대체 왜 본사이트에서는 에러가 안나는지 신기할 정도임
+// 아마 mongoose 버젼 차이 때문이 아닌가 의심됨 하여튼 뭔가 버젼 차이가 있는 것 같음
+import attachMiddlewares from './middlewares/index';
 import './libs/setupPassport';
+
 
 // Load some schemas & models
 import './models/challenge';
